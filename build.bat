e70f07dc1cecd76121321baf3a1ad1ac15e7c662
Choo Zhi Min <choozhimin@gmail.com>
2016-06-02 15:36:45 +0800

    add the build and run script without the need of Eclipse to build & run.

---
+++ build.bat
@@ -0,0 +1,1 @@
+javac -d bin -cp lib\commons-csv-1.3.jar;lib\selenium-server-standalone-2.53.0.jar src\openpath\*.java
---
+++ run.bat
@@ -0,0 +1,2 @@
+rem Make sure you copy src\openpath\resources to bin\openpath
+java -cp bin openpath.MainFrame
